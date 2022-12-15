f64f7bf8ff7b747aa5d11faedc8695deff737e51
AndrewBurUU <andriv80@gmail.com>
2022-12-15 17:39:25 +0800

    0. Что такое атомарность? [#6856 #371442]

---
+++ src/main/java/ru/job4j/atomic/Cache.java
@@ -0,0 +1,27 @@
+package ru.job4j.atomic;
+
+public final class Cache {
+    private static Cache cache;
+
+    public static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Cache cache = new Cache();
+        Thread first = new Thread(
+                () -> cache.instOf()
+        );
+        Thread second = new Thread(
+                () -> cache.instOf()
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        System.out.println(cache);
+    }
+}
