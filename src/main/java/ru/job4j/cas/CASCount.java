9c5851409160737250f356673e8ec3fd45903f61
AndrewBurUU <andriv80@gmail.com>
2022-12-27 11:46:26 +0800

    0. CAS - операции [#6859 #374862]

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -6,12 +6,15 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
         int current;
         int next;
         do {
+            if (count.get() == null) {
+                throw new UnsupportedOperationException("Count is not impl.");
+            }
             current = count.get();
             next = current + 1;
         } while (!count.compareAndSet(current, next));
