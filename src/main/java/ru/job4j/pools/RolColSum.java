3a4d6a76f92789ccfceeb8c6d0d8c91d659364cb
AndrewBurUU <andriv80@gmail.com>
2023-01-10 08:56:58 +0800

    4. CompletableFuture [#361626 #379413]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,5 +1,7 @@
 package ru.job4j.pools;
 
+import java.util.concurrent.*;
+
 public class RolColSum {
     public static class Sums {
         private int rowSum;
@@ -42,13 +44,47 @@ public class RolColSum {
         return res;
     }
 
-    public static Sums[] asyncSum(int[][] matrix) {
-        return new Sums[matrix.length];
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        int n = matrix.length;
+        Sums[] res = new Sums[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = new Sums();
+        }
+        for (int i = 0; i < n; i++) {
+            res[i].setRowSum(getRowSum(matrix, 0, n, i).get());
+            res[i].setColSum(getColSum(matrix, 0, n, i).get());
+        }
+        return res;
     }
 
-    public static void main(String[] args) {
+    public static CompletableFuture<Integer> getRowSum(int[][] data, int startCol, int endCol, int row) {
+        return CompletableFuture.supplyAsync(() -> {
+            int sum = 0;
+            for (int j = startCol; j < endCol; j++) {
+                sum += data[row][j];
+                }
+                return sum;
+            }
+       );
+    }
+
+    public static CompletableFuture<Integer> getColSum(int[][] data, int startRow, int endRow, int col) {
+        return CompletableFuture.supplyAsync(() -> {
+                    int sum = 0;
+                    for (int i = startRow; i < endRow; i++) {
+                        sum += data[i][col];
+                    }
+                    return sum;
+                }
+        );
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         int[][] array = {{5, 1, 6}, {8, 2, 3}, {4, 1, 0}};
-        Sums[] res = RolColSum.sum(array);
+        /** Sequence
+        * Sums[] res = RolColSum.sum(array);*/
+        /** Async */
+        Sums[] res = RolColSum.asyncSum(array);
         System.out.println("RowSum:");
         for (int i = 0; i < res.length; i++) {
             System.out.println(res[i].getRowSum());
--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -2,6 +2,8 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.*;
+
 import static org.assertj.core.api.Assertions.*;
 
 class RolColSumTest {
@@ -10,6 +12,18 @@ class RolColSumTest {
     void whenSequence() {
         int[][] array = {{5, 1, 6}, {8, 2, 3}, {4, 1, 0}};
         RolColSum.Sums[] res = RolColSum.sum(array);
+        assertThat(res[0].getRowSum()).isEqualTo(12);
+        assertThat(res[1].getRowSum()).isEqualTo(13);
+        assertThat(res[2].getRowSum()).isEqualTo(5);
+        assertThat(res[0].getColSum()).isEqualTo(17);
+        assertThat(res[1].getColSum()).isEqualTo(4);
+        assertThat(res[2].getColSum()).isEqualTo(9);
+    }
+
+    @Test
+    void whenAsync() throws ExecutionException, InterruptedException {
+        int[][] array = {{5, 1, 6}, {8, 2, 3}, {4, 1, 0}};
+        RolColSum.Sums[] res = RolColSum.asyncSum(array);
         assertThat(res[0].getRowSum()).isEqualTo(12);
         assertThat(res[1].getRowSum()).isEqualTo(13);
         assertThat(res[2].getRowSum()).isEqualTo(5);
